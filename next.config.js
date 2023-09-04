acffc0a8a79bc3531d160da7d989a549ab813d8d
Derian Pinto <pintoderian@hotmail.com>
2023-09-04 17:55:13 -0500

    Next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'media.licdn.com',
-        port: '',
-        pathname: '/dms/image/**'
-      }
-    ]
-  }
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
