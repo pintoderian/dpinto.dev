a16b792a89e42e66604da578d77d5ad18ebd085f
Derian Pinto <pintoderian@hotmail.com>
2024-02-11 18:33:35 -0500

    Separator in menu

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -7,9 +7,9 @@ import Menu from "./_children/menu";
 export default function Header() {
   return (
     <header className="fixed top-0 z-10 flex items-center justify-center w-full mx-auto mt-2">
-      <nav className="flex px-4 py-2 text-sm font-medium rounded-full text-gray-600 dark:text-gray-200 justify-center items-center gap-4 lg:gap-14">
-        <Menu />
-        <div className="flex flex-row justify-end gap-4">
+      <nav className="flex px-4 py-2 text-sm font-medium rounded-full text-gray-600 dark:text-gray-200 justify-center items-center divide-x divide-gray-300 dark:divide-neutral-500">
+        <Menu className="pr-4" />
+        <div className="flex flex-row justify-end gap-4 pl-4">
           <BtnLanguage />
           <BtnTheme />
         </div>
